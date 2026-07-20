23ba9178df8514b359836314e4421f3fae8d482c
Muaaz Raza <yourssharky@gmail.com>
2026-07-20 14:09:15 +0500

    process tr data and build a self attention block with multi heads

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-def main():
-    print("Hello from microgpt!")
+import numpy as np 
+from process_data import load_essentials , get_target_labels
+
+block_size = 8 
+batch_size = 32
+heads = 4
+embedding_d,query_wei_d,value_wei_d = 16 , int(32/heads) ,int(64/heads)
+train_set,test_set ,itos,stoi,encode , decode = load_essentials()
+x,y = get_target_labels(block_size,train_set,block_size)
+
+def softmax(x):
+    return np.exp(x)/np.sum(np.exp(x),axis=1,keepdims=True)
+
+# embedding table of the model (vocabulary size, embedding dimension)
+token_embedding_table = np.random.randn(len(itos),embedding_d) * ( 1 / (embedding_d)**0.5)
+
+# coverting tokens to embedding vectors
+input = np.stack([token_embedding_table[xi] for xi in x[0]]) # dim(input) = number of tokens in input * embedding_dimension
+attentions_blocks = []
+rng = np.random.default_rng()
+Wo = rng.standard_normal((value_wei_d*heads, embedding_d)) * (1 / value_wei_d**0.5)
+Wq = rng.standard_normal((heads,embedding_d, query_wei_d)) * (1 / embedding_d**0.5)
+Wk = rng.standard_normal((heads,embedding_d, query_wei_d)) * (1 / embedding_d**0.5)
+Wv = rng.standard_normal((heads,embedding_d, value_wei_d)) * (1 / embedding_d**0.5)
+
+for i in range(heads) :
+    Q = input @ Wq[i]
+    K = input @ Wk[i]
+    V = input @ Wv[i]
+
+    Qk = Q @ K.T
+    Qk /= (query_wei_d)**0.5
+    mask = np.tril(np.ones((block_size)))
+    mask[mask==0] = -np.inf 
+    mask[mask==1] = 0 
+    Qk += mask 
+
+    Attention = softmax(Qk) @ V 
+    attentions_blocks.append(Attention)
+
+attentions = np.concatenate(attentions_blocks,axis=1)    
+final = attentions @ Wo
+print(final.shape)
 
 
-if __name__ == "__main__":
-    main()
+
+
+
---
+++ process_data.py
@@ -0,0 +1,51 @@
+import numpy as np 
+
+def load_vocabulary(data):
+    vocab = sorted(list(set("".join(data))))
+    itos = {i:char for i,char in enumerate(vocab)}
+    stoi = {char:i for i,char in enumerate(vocab)}
+    return itos,stoi
+
+def load_essentials():
+    # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+    with open("input.txt") as file:
+        data = file.read().splitlines()
+    n = int(0.9*len(data))
+    # 90% data ~ trainset 
+    train_set = data[:n]
+    test_set = data[n:]
+    # index to string , string to index lookup tables
+    itos,stoi = load_vocabulary(data)
+
+    # index to string , string to index functions 
+    encode = lambda x:[stoi[char] for char in "".join(x)]
+    decode = lambda x:"".join([itos[index] for index in x])
+
+    # encode the whole data set splits
+    return np.array(encode(train_set),dtype=np.long),np.array(encode(test_set),dtype=np.long) ,itos,stoi ,encode,decode
+
+
+
+def get_target_labels(block_size,data,batch_size):
+    rng = np.random.default_rng()
+    # 'batch size' number of random offset in dataset 
+    indices = rng.integers(high=len(data)-block_size-1,low=0,size=batch_size)
+
+    # stacking up all the data blocks produced by random indices
+    
+    x =np.stack([data[i:i+block_size] for i in indices])
+    y =np.stack([data[i+1:i+block_size+1] for i in indices])
+    # dim(x) = dim(y) = (batch_size=number of examples,block_size=number of tokens in a block per example)
+    return x,y 
+
+
+
+
+
+
+
+
+
+
+
+
