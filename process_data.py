bee50ea41c0fc3d654463f0ec44654d6596a5967
mmuaaz-raza <yourssharky@gmail.com>
2026-09-26 08:54:35 +0500

    fix target training file

--- process_data.py
+++ process_data.py
@@ -8,7 +8,7 @@ def load_vocabulary(data):
 
 def load_essentials():
     # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-    with open("data2.txt") as file:
+    with open("input.txt") as file:
         data = file.read()
     n = int(0.9*len(list(data)))
     # 90% data ~ trainset 
